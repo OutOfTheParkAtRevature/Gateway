a4501b77cdcaea4459a29d5350effdbf1c7424a8
ranerijosh <43115830+ranerijosh@users.noreply.github.com>
2021-03-03 12:58:14 -0500

    Update Startup.cs

--- Gateway/Startup.cs
+++ Gateway/Startup.cs
@@ -31,8 +31,7 @@ namespace Gateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<Router>();
-            services.AddScoped<Destination>();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
